ab740e7ca0fd510517a8ce4fcc6abf2be20c97c8
ananyaanagarsheth <ananyaa.nagarsheth@republic.co>
2024-06-25 15:26:06 -0400

    tried new thing to add resume

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -46,7 +46,7 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="/Resume.pdf"
+            href="/Users/ananyaa.nagarsheth/Desktop/blog/Resume.pdf"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">Resume</p>
