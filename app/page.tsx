3e02a8f4e1a7d47f90656883016f90f4f5669869
ananyaanagarsheth <ananyaa.nagarsheth@republic.co>
2024-06-25 14:03:23 -0400

    changes to the main decsription and title

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,11 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        Ananyaa Nagarsheth
       </h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        {`I'm a rising junior at the University of Illinois at Urbana-Champaign, pursuing 
+        a degree in materials science and engineering with a minor in Mathematics. `}
       </p>
       <div className="my-8">
         <BlogPosts />
