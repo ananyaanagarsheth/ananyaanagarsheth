9656c39d4369f0c3d24c672807fd850e5ecaad61
ananyaanagarsheth <ananyaa.nagarsheth@republic.co>
2024-06-27 12:16:24 -0400

    changes to the homeaoge typos

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,32 @@
-
-
 export default function Page() {
   return (
-    <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+    <section className="grid grid-cols-2 container mx-auto" >
+      <div className="col-span-2">
+
+      
+      <h1 className="mb-8 text-2xl font-semibold tracking-normal">
         Ananyaa Nagarsheth
       </h1>
-      <p className="mb-4">
+      <p className="mb-4 tracking-normal ">
         {`I'm a rising junior at the University of Illinois at Urbana-Champaign, pursuing 
         a degree in materials science and engineering with a minor in Mathematics. `}
       </p>
-      <div className="my-8">
-        <img src="uiuc.jpeg" alt="uni photo">
-        </img>
       </div>
-      <div>
-        <h2 className='text-2xl mb-3'>Skills</h2>
+      <div className="col-span-2">
+      <h2 className='text-2xl mb-3'>Skills</h2>
        <ul className="mb-4">
-          <li>CAD-Solidwords</li>
+          <li>CAD-Solidworks</li>
           <li>Programming: HTML, CSS, Github, Python, R, Javascript,
                            React, Next.js, Vercel
           </li>
           </ul>
       </div>
+
+      <div className="my-8 col-span-1">
+        <img src="uiuc.jpeg" alt="uni photo" className="col-span-2 float-right">
+        </img>
+      </div>
+      
     </section>
   )
 }
